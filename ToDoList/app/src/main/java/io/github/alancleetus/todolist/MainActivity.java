92cd0b6fa5419c855d0073ba076d477b84538a03
alancleetus <alanclee95@gmail.com>
2017-07-12 22:39:12 -0700

    changed the look of things and overall just made everything more efficient

--- ToDoList/app/src/main/java/io/github/alancleetus/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/io/github/alancleetus/todolist/MainActivity.java
@@ -1,80 +1,43 @@
 package io.github.alancleetus.todolist;
 
+import android.content.res.ColorStateList;
 import android.database.Cursor;
-import android.graphics.Typeface;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
     //defining variables
-    private Button SubmitBtn;
-    private EditText InputBox;
     private LinearLayout ParentLayoutDone;
     private LinearLayout ParentLayoutNotDone;
     private DatabaseHelper myDb;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         /****My code starts here*****/
 
-        //initializing variables
-        myDb = new DatabaseHelper(this);
-        SubmitBtn = (Button) findViewById(R.id.submitButton);
-        InputBox = (EditText) findViewById(R.id.newItemInputBox);
-        ParentLayoutDone = (LinearLayout) findViewById(R.id.doneSection);
-        ParentLayoutNotDone = (LinearLayout) findViewById(R.id.toBeDoneSection);
-
-
-        /****load saved data***/
-        load();
-
-        /***this is what happens when the submit button is clicked***/
-        SubmitBtn.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //if the user has not written anything, return
-                if (InputBox.getText().toString().matches("")) return;
-
-                //save item to database
-                long id = myDb.insert(InputBox.getText().toString());
-
-                //add item to list on app
-                addToDoList(id, InputBox.getText().toString(), "general");
-
-                //clear the input box
-                InputBox.setText("");
-            }
-        });
-
-        /***this is what happens when the button is long pressed***/
-        SubmitBtn.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-
                 final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
                 View mView = getLayoutInflater().inflate(R.layout.dialog, null);
                 final EditText task = (EditText) mView.findViewById(R.id.TaskInput);
                 final EditText type = (EditText) mView.findViewById(R.id.TypeInput);
                 Button addButton = (Button) mView.findViewById(R.id.NewTaskButton);
-
-                if (!InputBox.getText().toString().matches("")) task.setText(InputBox.getText());
 
                 addButton.setOnClickListener(new View.OnClickListener(){
 
@@ -82,8 +45,9 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View view)
                     {
                         //if the user has not written anything, return
-                        if (task.getText().toString().matches("") || type.getText().toString().matches("")) return;
+                        if (task.getText().toString().matches("") ) return;
 
+                        if(type.getText().toString().matches("")) type.setText("general");
                         //save item to database
                         long id = myDb.insert(task.getText().toString(), type.getText().toString());
 
@@ -100,44 +64,23 @@ public class MainActivity extends AppCompatActivity {
                 alertBuilder.setView(mView);
                 AlertDialog dialog = alertBuilder.create();
                 dialog.show();
-
-                return true;
             }
         });
 
+        //initializing variables
+        myDb = new DatabaseHelper(this);
+        ParentLayoutDone = (LinearLayout) findViewById(R.id.doneSection);
+        ParentLayoutNotDone = (LinearLayout) findViewById(R.id.toBeDoneSection);
 
-
-        /****what happens when enter key is presses on the edit text***/
-        InputBox.setOnKeyListener(new View.OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                // If the event is a key-down event on the "enter" button
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-
-                    //if the user has not written anything, return
-                    if (InputBox.getText().toString().matches("")) return true;
-
-                    //save item to database
-                    long id = myDb.insert(InputBox.getText().toString());
-
-                    //add item to list on app
-                    addToDoList(id, InputBox.getText().toString(), "general");
-
-                    //clear the input box
-                    InputBox.setText("");
-
-                    return true;
-                }
-                return false;
-            }
-        });
-
-
+        /****load saved data***/
+        load();
     }
 
     /**
      * loads all the task when the app is first opened
      **/
-    public void load() {
+    public void load()
+    {
         Cursor data = myDb.getAllData();
 
         while (data.moveToNext()) {
@@ -148,24 +91,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void addToDoList(long id, final String newItem, final String type) {
-        final CheckBox toDoItem = new CheckBox(getApplicationContext());
+    public void addToDoList(long id, final String newItem, final String type)
+    {
+        final View toDoItem = getLayoutInflater().inflate(R.layout.taskholder, null);
+        final TextView task = (TextView) toDoItem.findViewById(R.id.taskTextView);
+        final TextView typeOfTask = (TextView) toDoItem.findViewById(R.id.topicTextView);
+        Button editButton = (Button) toDoItem.findViewById(R.id.editButtonForTask);
 
-        toDoItem.setText(newItem);
-        toDoItem.setTag(id);
-        toDoItem.setLongClickable(true);
+        editButton.setBackgroundTintList(ColorStateList.valueOf( Color.GREEN ));
 
-        //styles for the items
-        toDoItem.setTextSize(16);
-        toDoItem.setTypeface(Typeface.create("casual", Typeface.NORMAL));
-        toDoItem.setPadding(2, 10, 2, 10);
-        toDoItem.setBackgroundResource(R.drawable.card);
+        editButton.setOnClickListener(new View.OnClickListener(){
 
-        //this is what happens when the to be done item is checked
-        toDoItem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+            public void onClick(View view)
+            {
                 //update the status to true
                 myDb.updateDataStatus(toDoItem.getTag().toString(), "true");
 
@@ -175,27 +114,94 @@ public class MainActivity extends AppCompatActivity {
                 //we then add it to the done section
                 addToDoneList(toDoItem);
 
-
             }
         });
 
-        toDoItem.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
+        task.setText(newItem);
+        typeOfTask.setText(type);
 
-                Toast.makeText(MainActivity.this, "long click", Toast.LENGTH_SHORT).show();
+        toDoItem.setTag(id);
+        toDoItem.setLongClickable(true);
+
+        View.OnLongClickListener doItemClicked = new View.OnLongClickListener(){
+
+            @Override
+            public boolean onLongClick(View view)
+            {
+                final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog, null);
+                final EditText taskInAlert = (EditText) mView.findViewById(R.id.TaskInput);
+                final EditText typeOfTaskinAlert = (EditText) mView.findViewById(R.id.TypeInput);
+                Button addButtonInAlert = (Button) mView.findViewById(R.id.NewTaskButton);
+
+                taskInAlert.setText(newItem);
+                typeOfTaskinAlert.setText(type);
+                addButtonInAlert.setText("edit");
+
+                addButtonInAlert.setOnClickListener(new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View view)
+                    {
+                        //if the user has not written anything, return
+                        if (taskInAlert.getText().toString().matches("") || typeOfTaskinAlert.getText().toString().matches("")) return;
+
+                        //save item to database
+                        myDb.updateData(toDoItem.getTag().toString(), taskInAlert.getText().toString(), typeOfTaskinAlert.getText().toString(), "false");
+
+                        task.setText(taskInAlert.getText().toString());
+
+                        //clear the input box
+                        taskInAlert.setText("");
+                        typeOfTaskinAlert.setText("");
+
+                    }
+
+                });
+
+                alertBuilder.setView(mView);
+                AlertDialog dialog = alertBuilder.create();
+                dialog.show();
                 return true;
             }
-        });
+        };
+
+        //this is what happens when the to be done item is checked
+        toDoItem.setOnLongClickListener(doItemClicked);
+        task.setOnLongClickListener(doItemClicked);
+        typeOfTask.setOnLongClickListener(doItemClicked);
 
         ParentLayoutNotDone.addView(toDoItem, 0);
     }
 
-    public void addToDoneList(final CheckBox toDoItem) {
-        //this is what happens when you check an item in the done section
-        toDoItem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    public void addToDoneList(final View toDoItem)
+    {
+        final TextView task = (TextView) toDoItem.findViewById(R.id.taskTextView);
+        final TextView typeOfTask = (TextView) toDoItem.findViewById(R.id.topicTextView);
+
+        Button editButton = (Button) toDoItem.findViewById(R.id.editButtonForTask);
+
+        editButton.setBackgroundResource(R.drawable.cross);
+        editButton.setBackgroundTintList(ColorStateList.valueOf( Color.RED ));
+
+        editButton.setOnClickListener(new View.OnClickListener(){
+
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            public void onClick(View view)
+            {
+                myDb.delete(""+ toDoItem.getTag());
+                ((LinearLayout) ParentLayoutDone).removeView(toDoItem);
+                Toast.makeText(MainActivity.this, "Task deleted", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        View.OnClickListener doneItemClicked = new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view)
+            {
                 //update the status to false
                 myDb.updateDataStatus(toDoItem.getTag().toString(), "false");
 
@@ -203,48 +209,53 @@ public class MainActivity extends AppCompatActivity {
                 ((LinearLayout) ParentLayoutDone).removeView(toDoItem);
 
                 //then we add it to the list again
-                addToDoList((long) toDoItem.getTag(), toDoItem.getText().toString(), "general");
+                addToDoList((long) toDoItem.getTag(), task.getText().toString(), typeOfTask.getText().toString());
             }
-        });
+        };
 
+        //this is what happens when you check an item in the done section
+        toDoItem.setOnClickListener(doneItemClicked);
+        task.setOnClickListener(doneItemClicked);
+        typeOfTask.setOnClickListener(doneItemClicked);
 
-        toDoItem.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-
-                myDb.delete(""+ toDoItem.getTag());
-                ((LinearLayout) ParentLayoutDone).removeView(toDoItem);
-                Toast.makeText(MainActivity.this, "Task deleted", Toast.LENGTH_SHORT).show();
-
-                return true;
-            }
-        });
-
-
-
-        ((LinearLayout) ParentLayoutDone).addView(toDoItem, 0);
+       ((LinearLayout) ParentLayoutDone).addView(toDoItem, 0);
 
     }
 
     public void addToDoneList(long id, final String newItem, final String type) {
 
-        final CheckBox toDoItem = new CheckBox(getApplicationContext());
 
-        toDoItem.setText(newItem);
+        final View toDoItem = getLayoutInflater().inflate(R.layout.taskholder, null);
+        final TextView task = (TextView) toDoItem.findViewById(R.id.taskTextView);
+        final TextView typeOfTask = (TextView) toDoItem.findViewById(R.id.topicTextView);
+        Button editButton = (Button) toDoItem.findViewById(R.id.editButtonForTask);
+
+        editButton.setBackgroundResource(R.drawable.cross);
+        editButton.setBackgroundTintList(ColorStateList.valueOf( Color.RED ));
+
+        editButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view)
+            {
+                myDb.delete(""+ toDoItem.getTag());
+                ((LinearLayout) ParentLayoutDone).removeView(toDoItem);
+                Toast.makeText(MainActivity.this, "Task deleted", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        task.setText(newItem);
+        typeOfTask.setText(type);
+
         toDoItem.setTag(id);
         toDoItem.setLongClickable(true);
-        toDoItem.setChecked(true);
 
-        //styles for the items
-        toDoItem.setTextSize(16);
-        toDoItem.setTypeface(Typeface.create("casual", Typeface.NORMAL));
-        toDoItem.setPadding(2, 10, 2, 10);
-        toDoItem.setBackgroundResource(R.drawable.card);
+        View.OnClickListener doneItemClicked = new View.OnClickListener(){
 
-        //this is what happens when you check an item in the done section
-        toDoItem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            public void onClick(View view)
+            {
                 //update the status to false
                 myDb.updateDataStatus(toDoItem.getTag().toString(), "false");
 
@@ -252,102 +263,18 @@ public class MainActivity extends AppCompatActivity {
                 ((LinearLayout) ParentLayoutDone).removeView(toDoItem);
 
                 //then we add it to the list again
-                addToDoList((long) toDoItem.getTag(), toDoItem.getText().toString(), type);
+                addToDoList((long) toDoItem.getTag(), task.getText().toString(), typeOfTask.getText().toString());
             }
-        });
+        };
 
-
-        toDoItem.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-
-                myDb.delete(""+ toDoItem.getTag());
-                ((LinearLayout) ParentLayoutDone).removeView(toDoItem);
-                Toast.makeText(MainActivity.this, "Task deleted", Toast.LENGTH_SHORT).show();
-
-                return true;
-            }
-        });
-
+        //this is what happens when you check an item in the done section
+        toDoItem.setOnClickListener(doneItemClicked);
+        task.setOnClickListener(doneItemClicked);
+        typeOfTask.setOnClickListener(doneItemClicked);
 
         ((LinearLayout) ParentLayoutDone).addView(toDoItem, 0);
 
     }
 
-    /****************************************************
-     * this function adds new to do list item to the    *
-     * linear layout and also the array list of         *
-     * all items                                        *
-     *                                                   *
-     ****************************************************/
-    public void addToList2(long id, final String newItem) {
-        if (!newItem.matches("")) {
-            final ListView toDoItem = new ListView(getApplicationContext());
-           /* GridLayout wrapper = new GridLayout(getApplicationContext(), null, R.style.wrapper);
-            TextView mainText = new TextView(getApplicationContext(), null, R.style.mainTextStyle);
-            TextView subText = new TextView(getApplicationContext(), null, R.style.subText);
-            Button doneButton = new Button(getApplicationContext(), null, R.style.doneButton);
-*/
-            GridLayout wrapper = (GridLayout) getLayoutInflater().inflate(R.layout.wrapper, null);
-            TextView mainText = (TextView) getLayoutInflater().inflate(R.layout.maintext, null);
-            TextView subText = (TextView) getLayoutInflater().inflate(R.layout.subtext, null);
-            Button doneButton = (Button) getLayoutInflater().inflate(R.layout.donebutton, null);
-
-            //styles for the items
-            toDoItem.setBackgroundResource(R.drawable.card);
-            mainText.setTypeface(Typeface.create("casual", Typeface.NORMAL));
-            subText.setTypeface(Typeface.create("casual", Typeface.NORMAL));
-
-
-            //this is what happens when the to be done item is checked
-            /*toDoItem.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View view)
-                {
-                    //first we remove it from the toDoItem array list
-                    removeFromList(toDoItem.getText().toString());
-
-                    //then we save the arraylist
-                    save();
-
-                    //then we remove the item from the to be done section
-                    ((LinearLayout) ParentLayoutDone).removeView(toDoItem);
-
-                    //we then add it to the done section
-                    ((LinearLayout) ParentLayoutNotDone).addView(toDoItem, 0);
-
-                    //this is what happens when you check an item in the done section
-                    toDoItem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-                    {
-                        @Override
-                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-                        {
-                            //first we remove it from the done section
-                            ((LinearLayout) ParentLayoutNotDone).removeView(toDoItem);
-
-                            //then we add it to the list again
-                            addToList(toDoItem.getText().toString());
-                            save();
-                        }
-                    });
-                    //Toast.makeText(MainActivity.this, toDoItem.getText()+"- marked done" , Toast.LENGTH_SHORT).show();
-                }
-            });
-*/
-            mainText.setText(newItem);
-            subText.setText("subText");
-            doneButton.setText(">");
-
-
-            wrapper.addView(mainText);
-            wrapper.addView(subText);
-            wrapper.addView(doneButton);
-
-
-            ParentLayoutDone.addView(wrapper, 0);
-
-        }
-    }
 
 }
